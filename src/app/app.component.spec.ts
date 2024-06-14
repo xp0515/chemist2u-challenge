f644e77ee4f77ea5fff201b742109b1876ffb64e
xp0515 <x6868p@163.com>
2024-06-14 22:07:45 +1000

    add github action file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { SocketService } from './shared/services/socket.service';
+
+const config: SocketIoConfig = { url: '54.90.121.83:3000', options: {} };
 
 describe('AppComponent', () => {
+  let service: SocketService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, SocketIoModule.forRoot(config)],
+      providers: [SocketService]
     }).compileComponents();
+    service = TestBed.inject(SocketService);
   });
 
   it('should create the app', () => {
@@ -20,10 +28,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('chemist2u-challenge');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, chemist2u-challenge');
-  });
 });
